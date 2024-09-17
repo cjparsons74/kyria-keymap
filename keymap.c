dd369b3bd34f9eb5219704e51ccba05f5162e7da
Chris Parsons <chris@cjp.email>
2024-09-17 13:37:24 +0100

    revert to previous

--- keymap.c
+++ keymap.c
@@ -45,16 +45,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 #define D_ALT MT(MOD_LALT, KC_D)
 #define F_GUI MT(MOD_LGUI, KC_F)
-#define M_GUI MT(MOD_LGUI, KC_M)
-#define N_ALT MT(MOD_LALT, KC_N)
 
     [_QWERTY] = LAYOUT(
       /* LT(KC_RALT, KC_ESC) */
-       KC_ESC, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_PIPE,
-      _______, KC_A,   KC_S,   D_ALT,  F_GUI,  KC_G,                                         KC_H,   KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-      _______, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   LT(_LOWER, KC_UP),   LT(_RAISE, KC_LEFT), KC_RGHT, KC_DOWN, N_ALT,  M_GUI,     KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
-      _______, KC_DEL, LT(_LOWER, KC_ENT), CTL_T(KC_SPC), SFT_T(KC_ENT),        LT(_RAISE, KC_ENT), LT(_LOWER, KC_SPC), LT(_LOWER, KC_TAB),  KC_BSPC, LGUI(LCTL(KC_Q))
-
+      KC_MS_BTN2,              KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_PIPE,
+      KC_LCTL,                 KC_A,   KC_S,   D_ALT,  F_GUI,  KC_G,                                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+      KC_LSFT,                 KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_UP,   KC_LEFT, KC_RGHT, KC_DOWN,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
+      KC_LGUI, KC_DEL, MT(MOD_LALT, KC_ENT), LT(_LOWER, KC_SPC), LT(_RAISE, KC_ESC), KC_MS_BTN1, LT(_RAISE, KC_SPC), LT(_LOWER, KC_TAB),  KC_BSPC, LGUI(LCTL(KC_Q))
     ),
 /*
  * Lower Layer: Symbols
