f105365d950d55a678eed179149582bedec693ad
Chris Parsons <chris@cjp.email>
2024-10-23 07:51:09 +0100

    Level 4 colmack

--- keymap.c
+++ keymap.c
@@ -33,13 +33,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * Base Layer:
  *
  * ,-------------------------------------------.                              ,-------------------------------------------.
- * |  ESC   |   Q  |   W  |   F  |   J  |   G  |                              |   Y  |   U  |   I  |   O  |   P  |  | \   |
+ * |  ESC   |   Q  |   W  |   F  |   P  |   G  |                              |   J  |   U  |   I  |   Y  |   ;  |  | \   |
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
- * |        |   A  |   R  |   S  |  T   |   D  |                              |   H  |   N  |   E  |   L  | ;  : |  ' "   |
+ * |        |   A  |   R  |   S  |  T   |   D  |                              |   H  |   N  |   E  |   L  |   O  |  ' "   |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
  * |        |   Z  |   X  |   C  |   V  |   B  |LwrUpt|RseLft|  |Right |Down  |K/ALT |M/GUI | ,  < | . >  | /  ? |  - _   |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        |      | Del  | Enter| Space| Enter|  | Enter| Space| Tab  | Bksp |      |
+ *                        |      | Del  | Enter| Space| Enter|  | Enter| Space| Tab  | Bksp | Lock |
  *                        |      |      |Lower | Ctrl | Shift|  | Raise| Lower|Lower |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
@@ -48,8 +48,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_QWERTY] = LAYOUT(
       /* LT(KC_RALT, KC_ESC) */
-      KC_MS_BTN2,              KC_Q,   KC_W,   KC_F,   KC_J,   KC_G,                                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_PIPE,
-      KC_LCTL,                 KC_A,   KC_R,   KC_S,   KC_T,   KC_D,                                         KC_H,    KC_N,    KC_E,    KC_L,    KC_SCLN, KC_QUOT,
+      KC_MS_BTN2,              KC_Q,   KC_W,   KC_F,   KC_P,   KC_G,                                         KC_J,    KC_U,    KC_I,    KC_Y,    KC_SCLN,    KC_PIPE,
+      KC_LCTL,                 KC_A,   KC_R,   KC_S,   KC_T,   KC_D,                                         KC_H,    KC_N,    KC_E,    KC_L,    KC_O, KC_QUOT,
       KC_LSFT,                 KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_UP,   KC_LEFT, KC_RGHT, KC_DOWN,  KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
       KC_LGUI, KC_DEL, MT(MOD_LALT, KC_ENT), LT(_LOWER, KC_SPC), LT(_RAISE, KC_ESC), KC_MS_BTN1, LT(_RAISE, KC_SPC), LT(_LOWER, KC_TAB),  KC_BSPC, LGUI(LCTL(KC_Q))
     ),
