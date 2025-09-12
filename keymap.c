9f2f39cac0271221587bb5a8592710cb8b1d1f79
Chris Parsons <cparsons@thoughtmachine.net>
2025-09-12 12:28:10 +0100

    checkpoint

--- keymap.c
+++ keymap.c
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       MS_BTN2, KC_Q,   KC_W,   KC_F,   KC_P,   KC_G,                                         KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,    KC_PIPE,
       MS_BTN1, KC_A,   KC_R,   KC_S,   KC_T,   KC_D,                                         KC_H,    KC_N,   KC_E,  KC_I,    KC_O, KC_QUOT,
       MS_BTN1, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,    KC_UP,   KC_LEFT, KC_RGHT, KC_DOWN, KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
-      QK_CAPS_WORD_TOGGLE, KC_DEL, KC_ENT, KC_SPC,  KC_ESC,                       MO(_RAISE), LT(_LOWER, KC_SPC), KC_TAB,  KC_BSPC, LGUI(LCTL(KC_Q))
+      _______, KC_DEL, KC_ENT, LT(_LOWER, KC_SPC),  LT(_RAISE, KC_ESC),                       MO(_RAISE), LT(_LOWER, KC_SPC), KC_TAB,  KC_BSPC, LGUI(LCTL(KC_Q))
     ),
 
     [_LOWER] = LAYOUT(
--- rules.mk
+++ rules.mk
@@ -1,6 +1,6 @@
 OLED_DRIVER_ENABLE = no   # Enables the use of OLED displays
 RGBLIGHT_ENABLE = no      # Enable keyboard RGB underglow
-CAPS_WORD_ENABLE = yes
+# CAPS_WORD_ENABLE = yes
 QUANTUM_LIB_SRC += i2c_master.c
 ifeq ($(DEBUG), yes)
 	MOUSEKEY_ENABLE = no
@@ -11,7 +11,7 @@ else
 	MOUSEKEY_ENABLE = yes
 	ENCODER_ENABLE = yes
 	CONSOLE_ENABLE = no
-	EXTRAKEY_ENABLE = yes
+	# EXTRAKEY_ENABLE = yes
 endif
 POINTING_DEVICE_ENABLE = yes
 POINTING_DEVICE_DRIVER = pimoroni_trackball
